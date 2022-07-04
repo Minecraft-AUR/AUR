5a6e30b67de501303682fcbc1e5fa17b04d948cc
Kawa1Ares <kawaiiares@tom.com>
2022-07-04 10:27:56 +0800

    PLease fix Sponge Bootstrap

--- sponge/src/main/java/com/mcaur/sponge/bootstrap/SpongeBootstrap.java
+++ sponge/src/main/java/com/mcaur/sponge/bootstrap/SpongeBootstrap.java
@@ -30,25 +30,25 @@ public class SpongeBootstrap {
     
     @Listener
     public void onConstructPlugin(final ConstructPluginEvent event) {
-        this.logger.info("Downloading AUR core and other dependencies...");
-        http.sync("https://aur-core.oss-cn-beijing.aliyuncs.com/aur/AUR-core.jar").get().getBody()
-                .toFolder(String.valueOf(configDirectory.getFileSystem()))
-                .start();
-        http.sync("https://aur-core.oss-cn-beijing.aliyuncs.com/aur/OkHttps3.jar").get().getBody()
-                .toFolder(String.valueOf(configDirectory.getRoot()))
-                .start();
-        http.sync("https://aur-core.oss-cn-beijing.aliyuncs.com/aur/gson-2.9.0.jar").get().getBody()
-                .toFolder(String.valueOf(configDirectory.getRoot()))
-                .start();
-        http.sync("https://aur-core.oss-cn-beijing.aliyuncs.com/aur/logback-classic-1.3.0-alpha16.jar").get().getBody()
-                .toFolder(String.valueOf(configDirectory.getRoot()))
-                .start();
-        this.logger.info("Download successful! Starting server...");
+
     }
 
     @Listener
-    public void onServerStarting(final StartingEngineEvent<Server> event) {
-
+    public void onServerStarting(final StartingEngineEvent<Server> event){
+        this.logger.info("Downloading AUR core and other dependencies...");
+        http.sync("https://aur-core.oss-cn-beijing.aliyuncs.com/aur/AUR-core.jar").get().getBody()
+                .toFolder(String.valueOf(configDirectory.getRoot()))//Broken
+                .start();
+        http.sync("https://aur-core.oss-cn-beijing.aliyuncs.com/aur/OkHttps3.jar").get().getBody()
+                .toFolder(String.valueOf(configDirectory.getRoot()))//Broken
+                .start();
+        http.sync("https://aur-core.oss-cn-beijing.aliyuncs.com/aur/gson-2.9.0.jar").get().getBody()
+                .toFolder(String.valueOf(configDirectory.getRoot()))//Broken
+                .start();
+        http.sync("https://aur-core.oss-cn-beijing.aliyuncs.com/aur/logback-classic-1.3.0-alpha16.jar").get().getBody()
+                .toFolder(String.valueOf(configDirectory.getRoot()))//Broken
+                .start();
+        this.logger.info("Download successful! Starting server...");
     }
 
     @Listener
